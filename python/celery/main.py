e4ec4622cff1d646d4ae470305780256bb91ea8e
Santiago Blanco <sblancov84@gmail.com>
2018-02-19 23:22:15 +0100

    feat(celery): Use requests for getting information

--- python/celery/main.py
+++ python/celery/main.py
@@ -1,4 +1,7 @@
-from tasks import sum
+from tasks import request
 
+url = 'https://en.wikipedia.org/w/api.php?action=query&titles=Main%20Page&prop=revisions&rvprop=content&format=json&formatversion=2'
+result = request.delay(url)
+response = result.get()
 
-sum.delay(10, 3)
+print(response.json())
--- python/celery/tasks.py
+++ python/celery/tasks.py
@@ -1,9 +1,25 @@
 from celery import Celery
+import requests
 
+config = {
+    'backend': 'rpc://',
+    'broker': 'amqp://localhost',
+}
 
-app = Celery('tasks', broker='amqp://localhost')
+app = Celery('tasks', **config)
+
+app.conf.update(
+    task_serializer='pickle',
+    accept_content=['pickle'],  # Ignore other content
+    result_serializer='pickle',
+)
 
 
 @app.task
 def sum(x, y):
     return x + y
+
+
+@app.task(ignore_result=False)
+def request(url):
+    return requests.get(url)
